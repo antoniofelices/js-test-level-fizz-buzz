46d5ba1b26a972ab204997c34a74640fac8e21cd
antoniofelices <antonio@studiomoare.com>
2025-05-20 11:47:35 +0200

    add listener form,

--- src/ui/listeners.js
+++ src/ui/listeners.js
@@ -1,4 +1,6 @@
-import { formFizzBuzz, numberToTest, displayData } from '@ui/selectors'
+import { formFizzBuzz, numberToTest } from '@ui/selectors'
+import { calculateFizzBuzz } from '@core/fizzBuzz'
+import printResult from '@ui/printResult'
 
 const sendForm = () => {
     formFizzBuzz.addEventListener('submit', (event) => {
@@ -6,10 +8,26 @@ const sendForm = () => {
 
         numberToTest = parseInt(document.querySelector('#number-to-test')).value
         const messageNoNumber = `No has introducido un numero`
+        let result = 0
+        const messageFizz = 'Fizz'
+        const messageBuzz = 'Buzz'
+        const messageFizzBuzz = 'FizzBuzz'
 
         if (isNaN(numberToTest))
             return (showErrorsDiv.innerHTML = messageNoNumber)
+
+        result = calculateFizzBuzz(numberToTest)
+        printResult(result)
     })
 }
 
 export default sendForm
+
+// Solo voy a tener un listener === cuando se envia el form.
+
+// Cuando se envia form:
+//  Se captura valor del input
+//  Se comprueba si es numero o.
+//    Si no lo es se muestra mensaje en pantalla. <- Hasta aca ui
+//    Si es numero se llama a la funcion calculateFizzBuzz
+//    Se llama a funcion print result y se modifica la lista. Se añade un elemento abajo de todo.
